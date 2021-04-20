5ca1f532af4c23d6e57c23f750ceca8aabb16dd9
peggy891029 <79505452+peggy891029@users.noreply.github.com>
2021-04-20 16:51:40 +0800

    w9練習
    w9練習

---
+++ w9/animate&skrollr/js/script.js
@@ -0,0 +1,11 @@
+// $(document).click(function(){	
+
+// $('.bind-data').html($("#test").val());
+// console.log($("#test").val());
+// 		});
+$(function(){
+$('form').change(
+	$('.bind-data').text($("#test").val())
+	console.log($("#test").val());
+	
+})
